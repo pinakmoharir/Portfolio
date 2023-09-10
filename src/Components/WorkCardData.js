1a3ef6f3c95f42917e3ed84ea7ab18a0e584f88b
pinakmoharir <119129793+pinakmoharir@users.noreply.github.com>
2023-09-10 16:57:26 +0530

    Update WorkCardData.js

--- src/Components/WorkCardData.js
+++ src/Components/WorkCardData.js
@@ -1,15 +1,15 @@
 //change these to change the background image of projects
 import Pro1 from "../Assets/Pro1.jpg";
-import pro2 from "../Assets/pro2.jpg";
+import Drumkit from "../Assets/Drumkit.jpg"
 import prop3 from "../Assets/pro3.jpg"
 
 const ProjectCardData=[
     {
-        imgsrc:pro2,// after importing put the like this to get the images 
-        title:"Example of the project website that is going to be shown.",
-        text:"it is the information about the specific project",
-        view:"here paste the like for view button",
-        source:"here paste the different url for source button. "
+        imgsrc:Drumkit,// after importing put the like this to get the images 
+        title:"Drum Kit.",
+        text:"It is a simple and usable drum kit made with the help of simple HTML,CSS and Javascript",
+        view:"https://portfolio-4kbfsqzqf-pinakmoharir.vercel.app/project",
+        source:"https://github.com/pinakmoharir/Drums.git "
     },
     //just by pasting and entering the info we can create differnt cards for projects
     {
