56ddea4f814f3b216acd36758a81d6b760db8757
pinakmoharir <119129793+pinakmoharir@users.noreply.github.com>
2023-11-28 17:03:59 +0530

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,23 +1,45 @@
 import "./Formstyles.css";
 
+
 import React from 'react'
 
 const Form = () => {
   return (
     <div className="form">
-        <form>
+        <form action="https://formspree.io/f/xnqkkzzw" method="POST">
             <label>Your Name</label>
-            <input type="text"></input>
+            <input 
+              type="text" 
+              name="username" 
+              placeholder="username" 
+              autoComplete="off" 
+              required>
+            </input>
             <label>Email</label>
-            <input type="email"></input> 
+            <input 
+              type="email"               
+              name="Email" 
+              placeholder="Email" 
+              autoComplete="off" 
+              required></input> 
             <label>Phone Number</label>
-            <input type="number"></input>
+            <input 
+              type="number"
+              name="Number" 
+              placeholder="Number" 
+              autoComplete="off" 
+              required></input>
             <label>Message</label>
-            <textarea rows="8" placeholder="Type Your message here"></textarea>
-            <button className="btn">Submit</button>
+            <textarea 
+              name="Message"
+              autoComplete="off" 
+              required
+              rows="8" 
+              placeholder="Type Your message here"></textarea>
+            <button className="btn" type="submit" value="send">Submit</button>
         </form>
     </div>
   )
 }
 
-export default Form
+export default Form;
